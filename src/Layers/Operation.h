368484b0509aa30d40cae62d7e7549c053a9d064
EthanSystem <ElementsPhoenix@outlook.com>
2022-06-04 02:10:07 +0800

    add 2022-06-03
    添加操作头文件；

---
+++ src/Layers/Operation.h
@@ -0,0 +1,16 @@
+//
+// Created by Ethan on 2022/06/03.
+//
+
+#ifndef LIFEMANAGEMENTSYSTEM_OPERATION_H
+#define LIFEMANAGEMENTSYSTEM_OPERATION_H
+
+
+class Operation {
+public:
+    operator thinking;
+
+};
+
+
+#endif //LIFEMANAGEMENTSYSTEM_OPERATION_H
